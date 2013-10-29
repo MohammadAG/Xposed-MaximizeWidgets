db0e1756a55be14f1c30757ea6c4b25b21fa9e3d
Mohammad Abu-Garbeyyeh <mohammad7410@gmail.com>
2013-10-29 15:26:26 +0200

    Fix indentation

--- src/com/mohammadag/maximizewidgets/MaximizeWidgets.java
+++ src/com/mohammadag/maximizewidgets/MaximizeWidgets.java
@@ -16,46 +16,46 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class MaximizeWidgets implements IXposedHookLoadPackage {
 
-		@Override
-		public void handleLoadPackage(LoadPackageParam lpparam)
-				throws Throwable {
-			if (!lpparam.packageName.equals("android"))
-				return;
-			
-			Class<?> KeyguardHostView = findClass("com.android.internal.policy.impl.keyguard.KeyguardHostView", lpparam.classLoader);
-			
-			XposedHelpers.findAndHookMethod(KeyguardHostView, "onFinishInflate", new XC_MethodHook() {
-				@Override
-				protected void afterHookedMethod(MethodHookParam param) {
-					Object object = XposedHelpers.getObjectField(param.thisObject, "mSlidingChallengeLayout");
-					XposedHelpers.callMethod(object, "showChallenge", false);
-				}
-			});
-			
-			XposedHelpers.findAndHookMethod(KeyguardHostView, "onScreenTurnedOn", new XC_MethodHook() {
-				@Override
-				protected void afterHookedMethod(MethodHookParam param) {
-					Object object = XposedHelpers.getObjectField(param.thisObject, "mSlidingChallengeLayout");
-					XposedHelpers.callMethod(object, "showChallenge", false);
-				}
-			});
-			
-			findAndHookMethod("com.android.internal.policy.impl.keyguard.SlidingChallengeLayout", lpparam.classLoader, "onMeasure", int.class, int.class, new XC_MethodHook() {
-				@Override
-				protected void afterHookedMethod(final MethodHookParam param) throws Throwable {
-					final View mExpandChallengeView = (View) getObjectField(param.thisObject, "mExpandChallengeView");
-					ViewGroup slidingChallengeLayout = (ViewGroup) param.thisObject;
-					final FrameLayout keyGuardHostView = (FrameLayout) slidingChallengeLayout.getParent();
-					if (mExpandChallengeView != null && slidingChallengeLayout != null && keyGuardHostView != null)
-						mExpandChallengeView.setOnLongClickListener(new OnLongClickListener() {
-							@Override
-							public boolean onLongClick(View v) {
-								mExpandChallengeView.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-								callMethod(keyGuardHostView, "dismiss");
-								return false;
-							}
-						});
-				}
-			});
-		}
+	@Override
+	public void handleLoadPackage(LoadPackageParam lpparam)
+			throws Throwable {
+		if (!lpparam.packageName.equals("android"))
+			return;
+
+		Class<?> KeyguardHostView = findClass("com.android.internal.policy.impl.keyguard.KeyguardHostView", lpparam.classLoader);
+
+		XposedHelpers.findAndHookMethod(KeyguardHostView, "onFinishInflate", new XC_MethodHook() {
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) {
+				Object object = XposedHelpers.getObjectField(param.thisObject, "mSlidingChallengeLayout");
+				XposedHelpers.callMethod(object, "showChallenge", false);
+			}
+		});
+
+		XposedHelpers.findAndHookMethod(KeyguardHostView, "onScreenTurnedOn", new XC_MethodHook() {
+			@Override
+			protected void afterHookedMethod(MethodHookParam param) {
+				Object object = XposedHelpers.getObjectField(param.thisObject, "mSlidingChallengeLayout");
+				XposedHelpers.callMethod(object, "showChallenge", false);
+			}
+		});
+
+		findAndHookMethod("com.android.internal.policy.impl.keyguard.SlidingChallengeLayout", lpparam.classLoader, "onMeasure", int.class, int.class, new XC_MethodHook() {
+			@Override
+			protected void afterHookedMethod(final MethodHookParam param) throws Throwable {
+				final View mExpandChallengeView = (View) getObjectField(param.thisObject, "mExpandChallengeView");
+				ViewGroup slidingChallengeLayout = (ViewGroup) param.thisObject;
+				final FrameLayout keyGuardHostView = (FrameLayout) slidingChallengeLayout.getParent();
+				if (mExpandChallengeView != null && slidingChallengeLayout != null && keyGuardHostView != null)
+					mExpandChallengeView.setOnLongClickListener(new OnLongClickListener() {
+						@Override
+						public boolean onLongClick(View v) {
+							mExpandChallengeView.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
+							callMethod(keyGuardHostView, "dismiss");
+							return false;
+						}
+					});
+			}
+		});
+	}
 }
