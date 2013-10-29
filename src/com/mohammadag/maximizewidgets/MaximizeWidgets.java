1f2c74a5c496f7e3f12b5328b1dc75aff5fa1e56
Mohammad Abu-Garbeyyeh <mohammad7410@gmail.com>
2013-10-29 15:40:18 +0200

    Fix formatting and add some try...catch statements

--- src/com/mohammadag/maximizewidgets/MaximizeWidgets.java
+++ src/com/mohammadag/maximizewidgets/MaximizeWidgets.java
@@ -17,8 +17,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class MaximizeWidgets implements IXposedHookLoadPackage {
 
 	@Override
-	public void handleLoadPackage(LoadPackageParam lpparam)
-			throws Throwable {
+	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 		if (!lpparam.packageName.equals("android"))
 			return;
 
@@ -27,34 +26,41 @@ public class MaximizeWidgets implements IXposedHookLoadPackage {
 		XposedHelpers.findAndHookMethod(KeyguardHostView, "onFinishInflate", new XC_MethodHook() {
 			@Override
 			protected void afterHookedMethod(MethodHookParam param) {
-				Object object = XposedHelpers.getObjectField(param.thisObject, "mSlidingChallengeLayout");
-				XposedHelpers.callMethod(object, "showChallenge", false);
+				Object object = getObjectField(param.thisObject, "mSlidingChallengeLayout");
+				callMethod(object, "showChallenge", false);
 			}
 		});
 
 		XposedHelpers.findAndHookMethod(KeyguardHostView, "onScreenTurnedOn", new XC_MethodHook() {
 			@Override
 			protected void afterHookedMethod(MethodHookParam param) {
-				Object object = XposedHelpers.getObjectField(param.thisObject, "mSlidingChallengeLayout");
-				XposedHelpers.callMethod(object, "showChallenge", false);
+				Object object = getObjectField(param.thisObject, "mSlidingChallengeLayout");
+				callMethod(object, "showChallenge", false);
 			}
 		});
 
 		findAndHookMethod("com.android.internal.policy.impl.keyguard.SlidingChallengeLayout", lpparam.classLoader, "onMeasure", int.class, int.class, new XC_MethodHook() {
 			@Override
 			protected void afterHookedMethod(final MethodHookParam param) throws Throwable {
-				final View mExpandChallengeView = (View) getObjectField(param.thisObject, "mExpandChallengeView");
-				ViewGroup slidingChallengeLayout = (ViewGroup) param.thisObject;
-				final FrameLayout keyGuardHostView = (FrameLayout) slidingChallengeLayout.getParent();
-				if (mExpandChallengeView != null && slidingChallengeLayout != null && keyGuardHostView != null)
-					mExpandChallengeView.setOnLongClickListener(new OnLongClickListener() {
-						@Override
-						public boolean onLongClick(View v) {
-							mExpandChallengeView.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
-							callMethod(keyGuardHostView, "dismiss");
-							return false;
-						}
-					});
+				try {
+					final View mExpandChallengeView = (View) getObjectField(param.thisObject, "mExpandChallengeView");
+					ViewGroup slidingChallengeLayout = (ViewGroup) param.thisObject;
+					final FrameLayout keyGuardHostView = (FrameLayout) slidingChallengeLayout.getParent();
+					if (mExpandChallengeView != null && slidingChallengeLayout != null && keyGuardHostView != null) {
+						mExpandChallengeView.setOnLongClickListener(new OnLongClickListener() {
+							@Override
+							public boolean onLongClick(View v) {
+								mExpandChallengeView.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS);
+								callMethod(keyGuardHostView, "dismiss");
+								return false;
+							}
+						});
+					}
+				} catch (NoSuchFieldError e) {
+					e.printStackTrace();
+				} catch (NoSuchMethodError e) {
+					e.printStackTrace();
+				}
 			}
 		});
 	}
